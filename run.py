8c962571e3a1ec72a637e67bd80815104509e873
Georgekrutoj <georgyisavin2012@gmail.com>
2024-09-22 21:00:41 +0300

    some changes

--- run.py
+++ run.py
@@ -3,9 +3,16 @@ from flask import render_template
 
 app = Flask(
     import_name=__name__,
+    static_folder='app/static',
     template_folder='app/templates'
 )
+
 
 @app.route('/')
 def main() -> str:
     return render_template('index.html')
+
+
+@app.route('/about')
+def about() -> str:
+    return render_template('about.html')
